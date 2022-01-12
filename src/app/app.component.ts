a46c3fc09dd4aff182608e25027e1d5154221308
di1202@ukr.net <12dima02>
2022-01-12 16:23:34 +0200

    add position checking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pwa';
+export class AppComponent implements OnInit{
+  getCoords!: any
+  watchCoords!: any
+
+  ngOnInit(): void {
+    this.getPosition()
+    this.watchPosition()
+  }
+
+  private getPosition() {
+    navigator.geolocation.getCurrentPosition((position => {
+      this.getCoords = position.coords
+    }), err => console.log(err))
+  }
+
+  private watchPosition() {
+      navigator.geolocation.watchPosition((position => {
+        this.watchCoords = position.coords
+      }),err => console.log(err), {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+      })
+  }
+
 }
