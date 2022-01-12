f024ac2efdacdf65a81c067fb8fc86c0b8b74bf7
di1202@ukr.net <12dima02>
2022-01-12 16:34:39 +0200

    add map

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'pwa'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pwa');
   });
 
   it('should render title', () => {
