dac4ec23b25705a2f1058554b1e2a2d9fac4d5e3
@dmytro.gnatyshyn <dmytro.gnatyshyn@cgm.com>
2022-05-09 12:04:45 +0300

    added manifest

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,7 +9,7 @@ import {AppService} from "../../../services/app.service";
 export class HeaderComponent implements OnInit {
   headerColor!: string;
   zone!: string;
-  
+
   constructor(private appService: AppService) { }
 
   ngOnInit(): void {
@@ -19,4 +19,22 @@ export class HeaderComponent implements OnInit {
     })
   }
 
+  updateManifest(): void {
+    var myDynamicManifest = {
+      "name": "Your Great Site",
+      "short_name": "Your Great Site short name",
+      "description": "Something dynamic",
+      "start_url": `${window.location.origin}`,
+      "background_color": "#000000",
+      "theme_color": "#0f4a73",
+      "icons": [
+
+        ]
+      }
+      const link = document.createElement("link");
+      link.rel = "manifest";
+      const stringManifest = JSON.stringify(myDynamicManifest);
+      link.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(stringManifest))
+      document.head.appendChild(link);
+    }
 }
